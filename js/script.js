ac46bf53b9e2c46bd01f72aa06769014ba8b8381
MatteoFilippini <matteofilippini8@gmail.com>
2022-01-26 15:18:54 +0100

    created message

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
-console.log('ok', Vue);
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
 
 const root = new Vue({
     el: '#root',
     data: {
-
+        message: 'Titolo della pagina',
     }
 })
